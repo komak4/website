0f80d4925462c6af70fb6e2e29da68fffa5e9482
komak4 <komcio96@gmail.com>
2017-11-04 15:12:06 +0100

    index-source

--- index-source.php
+++ index-source.php
@@ -1,77 +1,99 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+    
     <title>Czytaj książki Online</title>
+    <meta charset="UTF-8">
 </head>
+    <style>
+        #logo-user {
+            background-color: #962D2D;  
+        }
+        
+        #logos-size {
+            width: 550px;
+            height: 750px;
+        }
+    
+    
+    </style>
 <body>
     <header>
     </header>
     <section id="introduction">
-        <div>
+        <div class="container text-center">
             <h1>Czytaj Książki online</h1>
             <p>
                 Skorzystaj z naszych usług, wypożyczaj i czytaj książki online.
-                Wybierz bezapelacyjnie najlepiej zoptymalizowane rozwiązanie
+                Wybierz bezapelacyjnie najlepiej zoptymalizowane<br> rozwiązanie
                 dostępne na rynku.
             </p>
         </div>
     </section>
     <section id="press">
-        <div>
-            <h1 class="">Mówią o nas</h1>
-            <div>
-                <div>
-                  <h2>Onet.pl</h2>
-                  <img src="http://www.kupujnasze.pl/wp-content/uploads/2010/03/logo_onetpl_kropka_odstep.jpg"/>
-                  <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-                  <p><a href="#" role="button">Czytaj więcej »</a></p>
-                </div>
-            <div>
-              <h2>wp.pl</h2>
-              <img src="https://d.wpimg.pl/1851656530-369356744/wp.png"/>
-              <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-              <p><a href="#" role="button">Czytaj więcej »</a></p>
-           </div>
-            <div>
-              <h2>Kurier UEK</h2>
-              <img src="http://cfm.uek.krakow.pl/media/images/8d/55/c032f96f30ebb23a22f96b221f90.jpeg"/>
-              <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-              <p><a href="#" role="button">Czytaj więcej »</a></p>
+        <div class="container">
+            <div class="container text-center">
+            <h1>Mówią o nas</h1>
             </div>
-          </div>
-        </div>
-       
+                <div class="container text-center">
+                    <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <h2>Onet.pl</h2>
+                  <img src="http://www.kupujnasze.pl/wp-content/uploads/2010/03/logo_onetpl_kropka_odstep.jpg" width=50%/>
+                  <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                  <button type="button" clas="btn btn-default">Czytaj więcej >></button> 
+                     </div>
+                        
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <h2>wp.pl</h2>
+                  <img src="https://d.wpimg.pl/1851656530-369356744/wp.png" width=50%/>
+                  <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                  <button type="button" clas="btn btn-default">Czytaj więcej >></button> 
+                  </div>
+                        
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <h2>Kurier UEK</h2>
+                  <img src="http://cfm.uek.krakow.pl/media/images/8d/55/c032f96f30ebb23a22f96b221f90.jpeg" width=50%/>
+                  <p>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym.   Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
+                <button type="button" clas="btn btn-default">Czytaj więcej >></button>      
+                    
+                  </div>     
+                    </div>
+                </div>
+            </div>
+        
     </section>
     <section id="stats">
-        <div>
+        <div class="container text-center">
             <h1>Statystyki</h1>
-            <div>
-                <div>
-                    <div>
-                        <i></i>
-                        <small>16k użytkowników dziennie</small>
-                    </div>
-                </div>
-                <div>
-                   <div>
-                        <i></i>
-                        <small>320k odsłon</small>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <i></i>
-                        <small>100k polubień</small>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <i></i>
-                        <small>1M pobrań</small>
-                    </div>
-                </div>
-            </div>
         </div>
+        <div class="container text-center" id="logos-size">
+            <div class="row">
+                            
+            <div class="col-sm-3" id="logo-user">    
+             <a href="#" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
+             <p>16k<br>użytkowników</p>
+            </div>
+                      
+            <div class="col-sm-3">    
+             <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+             <p>320k odsłon</p>
+            </div>    
+                        
+             <div class="col-sm-3">    
+              <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+              <p>100k polubień</p>
+             </div>  
+                            
+             <div class="col-sm-3">    
+              <span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+              <p>1M pobrań</p>
+             </div>
+                            
+           </div>
+   </div>
     </section>
     <section id="pricing">
         <div>
